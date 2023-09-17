14441f7afe1874bd4832c0d72d9bc206d5683b39
Bence Ditrich <ditrichb@gmail.com>
2023-09-17 18:12:54 +0200

    Minor component adjustments

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,17 +1,12 @@
 import { Component } from '@angular/core';
 
-interface FooterLink {
-  title: string;
-  url: string;
-}
-
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent {
-  leftColumnLinks: FooterLink[] = [
+  leftColumnLinks: { title: string; url: string }[] = [
     { title: 'About Coco Casing&trade;', url: '/about' },
     { title: 'FAQ', url: '/faq' },
     { title: 'Privacy Policy', url: '/' }, // TODO: Add URL
@@ -20,11 +15,29 @@ export class FooterComponent {
     { title: 'Terms of Service', url: '/' }, // TODO: Add URL
   ];
 
-  rightColumnLinks: FooterLink[] = [
+  rightColumnLinks: { title: string; url: string }[] = [
     { title: 'Home', url: '/' },
     { title: 'Products', url: '/products' },
     { title: 'Contact', url: '/contact' },
     { title: 'Blog', url: '/blog' },
+  ];
+
+  socialMediaLinks: { title: string; imageUrl: string; siteUrl: string }[] = [
+    {
+      title: 'Link to our Instagram profile',
+      imageUrl: '/assets/img/footer/instagram.svg',
+      siteUrl: 'https://www.instagram.com/coco.casing/',
+    },
+    {
+      title: 'Link to our TikTok profile',
+      imageUrl: '/assets/img/footer/tiktok.svg',
+      siteUrl: 'https://www.tiktok.com/@cococasing',
+    },
+    {
+      title: 'Link to our YouTube channel',
+      imageUrl: '/assets/img/footer/youtube.svg',
+      siteUrl: 'https://www.youtube.com/@cococasing',
+    },
   ];
 
   paymentOptionImages: { src: string; alt: string }[] = [
