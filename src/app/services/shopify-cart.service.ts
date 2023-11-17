f8e777704e9d4a679f1cf52255793ab1612c0e3c
Bence Ditrich <ditrichb@gmail.com>
2023-11-17 19:44:40 +0100

    Shopify services refactored

--- src/app/services/shopify-cart.service.ts
+++ src/app/services/shopify-cart.service.ts
@@ -1,19 +1,16 @@
 import { Injectable } from '@angular/core';
 import { map, switchMap } from 'rxjs';
 import { catchAndReportError } from '../common/utils/catch-and-report-error.operator';
-import { LineItem, ShoppingCart } from '../models/shopping-cart.model';
 import {
   AddLineItemGQL,
   CartGQL,
   CreateCartGQL,
   RemoveLineItemGQL,
   SetLineItemQuantityGQL,
-  ShoppingCartFragment,
 } from '../graphql/types';
-import { expectedProductOptions } from '../models/product.model';
 import { LocationService } from './location.service';
 import { NotificationService } from './notification.service';
-import { ShopifyProductService } from './shopify-product.service';
+import { mapCart } from '../common/utils/shopify-cart-helpers';
 
 @Injectable({
   providedIn: 'root',
@@ -22,7 +19,6 @@ export class ShopifyCartService {
   constructor(
     private readonly locationService: LocationService,
     private readonly notificationService: NotificationService,
-    private readonly shopifyProductService: ShopifyProductService,
     private readonly addLineItemGQL: AddLineItemGQL,
     private readonly removeLineItemGQL: RemoveLineItemGQL,
     private readonly cartGQL: CartGQL,
@@ -38,9 +34,7 @@ export class ShopifyCartService {
             variantId,
             countryCode,
           })
-          .pipe(
-            map((response) => this.mapCart(response.data?.cartCreate?.cart))
-          )
+          .pipe(map((response) => mapCart(response.data?.cartCreate?.cart)))
       ),
       catchAndReportError(this.notificationService)
     );
@@ -49,14 +43,14 @@ export class ShopifyCartService {
   fetchCart(cartId: string) {
     return this.cartGQL.fetch({ cartId }).pipe(
       map((response) => response.data?.cart),
-      map((cart) => (cart ? this.mapCart(cart) : null)),
+      map((cart) => (cart ? mapCart(cart) : null)),
       catchAndReportError(this.notificationService)
     );
   }
 
   addLineItem(cartId: string, variantId: string) {
     return this.addLineItemGQL.mutate({ cartId, variantId }).pipe(
-      map((response) => this.mapCart(response.data?.cartLinesAdd?.cart)),
+      map((response) => mapCart(response.data?.cartLinesAdd?.cart)),
       catchAndReportError(this.notificationService)
     );
   }
@@ -65,121 +59,15 @@ export class ShopifyCartService {
     return this.setLineItemQuantityGQL
       .mutate({ cartId, itemId, quantity })
       .pipe(
-        map((response) => this.mapCart(response.data?.cartLinesUpdate?.cart)),
+        map((response) => mapCart(response.data?.cartLinesUpdate?.cart)),
         catchAndReportError(this.notificationService)
       );
   }
 
   removeLineItem(cartId: string, itemId: string) {
     return this.removeLineItemGQL.mutate({ cartId, itemId }).pipe(
-      map((response) => this.mapCart(response.data?.cartLinesRemove?.cart)),
+      map((response) => mapCart(response.data?.cartLinesRemove?.cart)),
       catchAndReportError(this.notificationService)
     );
-  }
-
-  private mapCart(
-    cartFragment: ShoppingCartFragment | null | undefined
-  ): ShoppingCart {
-    if (!cartFragment) {
-      throw new Error('Failed to map cart as it was unavailable');
-    }
-
-    const lines: LineItem[] = cartFragment.lines.nodes.map(
-      (shopifyLineItem) => {
-        const {
-          merchandise: {
-            id: productId,
-            product,
-            selectedOptions,
-            image: productImage,
-          },
-        } = shopifyLineItem;
-
-        const allProductSlugs = [...expectedProductOptions.keys()];
-        const productSlug = allProductSlugs.find((slug) =>
-          product.handle.includes(slug)
-        );
-
-        if (!productSlug) {
-          throw new Error('Unrecognizable product handle: ' + product.handle);
-        }
-
-        const { serie, color, tier } =
-          this.shopifyProductService.parseProductVariantAttributes(
-            productSlug,
-            product.handle
-          );
-
-        let model = selectedOptions
-          .find((option) => option.name.toLowerCase().includes('model'))
-          ?.value.replaceAll(' ', '-')
-          .toLocaleLowerCase()
-          .replace(`iphone-${serie}`, '');
-
-        if (model?.startsWith('-')) {
-          model = model.substring(1);
-        }
-
-        const subtitleParts = [];
-
-        if (color) {
-          subtitleParts.push(color);
-        }
-
-        if (tier) {
-          subtitleParts.push(tier);
-        }
-
-        const lineItem: LineItem = {
-          id: shopifyLineItem.id,
-          product: {
-            id: productId,
-            title: `${productSlug}-iphone-${serie}${model ? `-${model}` : ''}`,
-            subtitle: subtitleParts.join('-'),
-            imageUrl: productImage?.url,
-          },
-          totalPrice: {
-            amount: parseFloat(shopifyLineItem.cost.totalAmount.amount),
-            currencyCode: shopifyLineItem.cost.totalAmount.currencyCode,
-          },
-          quantity: shopifyLineItem.quantity,
-        };
-
-        const lineItemOriginalTotalPrice = {
-          amount: parseFloat(shopifyLineItem.cost.subtotalAmount.amount),
-          currencyCode: shopifyLineItem.cost.subtotalAmount.currencyCode,
-        };
-
-        if (lineItem.totalPrice.amount < lineItemOriginalTotalPrice.amount) {
-          lineItem.originalTotalPrice = lineItemOriginalTotalPrice;
-        }
-
-        return lineItem;
-      }
-    );
-
-    const cart: ShoppingCart = {
-      id: cartFragment.id,
-      checkoutUrl: cartFragment.checkoutUrl,
-      lines,
-      totalPrice: {
-        amount: parseFloat(cartFragment.cost.totalAmount.amount),
-        currencyCode: cartFragment.cost.totalAmount.currencyCode,
-      },
-      totalQuantity: cartFragment.totalQuantity,
-    };
-
-    const cartOriginalTotalPrice = {
-      amount: lines
-        .map((lineItem) => lineItem.originalTotalPrice || lineItem.totalPrice)
-        .reduce((sum, currentPrice) => sum + currentPrice.amount, 0),
-      currencyCode: cartFragment.cost.totalAmount.currencyCode,
-    };
-
-    if (cart.totalPrice.amount < cartOriginalTotalPrice.amount) {
-      cart.originalTotalPrice = cartOriginalTotalPrice;
-    }
-
-    return cart;
   }
 }
--- src/app/services/shopify-product.service.ts
+++ src/app/services/shopify-product.service.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import { Injectable } from '@angular/core';
 import {
   Observable,
@@ -12,17 +11,7 @@ import {
   take,
   tap,
 } from 'rxjs';
-import {
-  FeaturedProduct,
-  Money,
-  Product,
-  ProductVariant,
-  defaultProductModel,
-  discountedProductTagPrefix,
-  expectedProductOptions,
-  productColors,
-  productTiers,
-} from '../models/product.model';
+import { FeaturedProduct, Money, Product } from '../models/product.model';
 import { LocationService } from './location.service';
 import { LocalStorageService } from './local-storage.service';
 import {
@@ -32,6 +21,11 @@ import {
 } from '../graphql/types';
 import { NotificationService } from './notification.service';
 import { catchAndReportError } from '../common/utils/catch-and-report-error.operator';
+import {
+  applyDiscount,
+  getDiscountPercentFromTags,
+  mapShopifyProductsToCocoProduct,
+} from '../common/utils/shopify-product-helpers';
 
 @Injectable({
   providedIn: 'root',
@@ -61,10 +55,12 @@ export class ShopifyProductService {
   }
 
   fetchProduct(productSlug: string): Observable<Product> {
+    const collectionHandle = `new-${productSlug}`;
+
     return this.locationService.getTwoLetterCountryCode().pipe(
       switchMap((countryCode) =>
         this.productGQL.fetch({
-          collectionHandle: `new-${productSlug}`,
+          collectionHandle,
           countryCode,
         })
       ),
@@ -81,91 +77,9 @@ export class ShopifyProductService {
 
         return response.data.collection.products.nodes;
       }),
-      map((shopifyProducts) => {
-        const variants = shopifyProducts.flatMap((shopifyProduct) => {
-          const { serie, color, tier } = this.parseProductVariantAttributes(
-            productSlug,
-            shopifyProduct.handle
-          );
-
-          const discountPercent = this.getDiscountPercentFromTags(
-            shopifyProduct.tags
-          );
-
-          const validShopifyProductVariants =
-            shopifyProduct.variants.nodes.filter((shopifyProductVariant) => {
-              const selectedModels = shopifyProductVariant.selectedOptions
-                .filter((option) =>
-                  option.name.toLocaleLowerCase().includes('model')
-                )
-                .map((option) => option.value);
-
-              return selectedModels.every(
-                (model) => model === selectedModels[0]
-              );
-            });
-
-          const productVariants = validShopifyProductVariants.map(
-            (shopifyProductVariant) => {
-              const modelTitle =
-                shopifyProductVariant.selectedOptions.find((option) =>
-                  option.name.toLowerCase().includes('model')
-                )?.value || '';
-
-              let model = modelTitle
-                .replaceAll(' ', '-')
-                .toLowerCase()
-                .replace(`iphone-${serie}`, '');
-
-              if (model.startsWith('-')) {
-                model = model.substring(1);
-              }
-
-              const slug = `iphone-${serie}${model ? `-${model}` : ''}${
-                color ? `-${color}` : ''
-              }${tier ? `-${tier}` : ''}`;
-
-              const productVariant: ProductVariant = {
-                id: shopifyProductVariant.id,
-                slug,
-                serie,
-                model: model || defaultProductModel,
-                color,
-                tier,
-                images: shopifyProduct.images.nodes.map((shopifyImage) => ({
-                  url: shopifyImage.url,
-                  altText: shopifyImage.altText || undefined,
-                })),
-                price: {
-                  amount: parseFloat(shopifyProductVariant.price.amount),
-                  currencyCode: shopifyProductVariant.price.currencyCode,
-                },
-              };
-
-              if (discountPercent) {
-                productVariant.originalPrice = {
-                  ...productVariant.price,
-                };
-
-                productVariant.price = this.applyDiscount(
-                  productVariant.price,
-                  discountPercent
-                );
-              }
-
-              return productVariant;
-            }
-          );
-
-          return productVariants;
-        });
-
-        return <Product>{
-          slug: productSlug,
-          description: '[Product description placeholder]', // TODO: Ask Nimer where to look for it
-          variants,
-        };
-      }),
+      map((shopifyProducts) =>
+        mapShopifyProductsToCocoProduct(shopifyProducts, productSlug)
+      ),
       catchAndReportError(this.notificationService)
     );
   }
@@ -195,7 +109,13 @@ export class ShopifyProductService {
             return of(response.data.product);
           }),
           map((shopifyProduct) => {
-            const price = shopifyProduct!.priceRange.minVariantPrice;
+            if (!shopifyProduct) {
+              throw new Error(
+                'Product at this stage should never be null or undefined'
+              );
+            }
+
+            const price = shopifyProduct.priceRange.minVariantPrice;
 
             return <Money>{
               amount: parseFloat(price.amount),
@@ -254,7 +174,7 @@ export class ShopifyProductService {
                 },
               };
 
-              const discountPercent = this.getDiscountPercentFromTags(
+              const discountPercent = getDiscountPercentFromTags(
                 shopifyFeaturedProduct.tags
               );
 
@@ -263,7 +183,7 @@ export class ShopifyProductService {
                   ...featuredProduct.price,
                 };
 
-                featuredProduct.price = this.applyDiscount(
+                featuredProduct.price = applyDiscount(
                   featuredProduct.price,
                   discountPercent
                 );
@@ -284,70 +204,5 @@ export class ShopifyProductService {
         )
       )
     );
-  }
-
-  parseProductVariantAttributes(productSlug: string, productHandle: string) {
-    const expectedOptions = expectedProductOptions.get(productSlug);
-
-    if (!expectedOptions) {
-      throw new Error(`Unrecognizable product handle: ${productHandle}`);
-    }
-
-    const handleWithoutProductSlug = productHandle.replace(
-      `${productSlug}-`,
-      ''
-    );
-
-    const handleParts = handleWithoutProductSlug.split('-');
-
-    if (handleParts.length < 2 || handleParts[0] !== 'iphone') {
-      throw new Error(`Unexpected product handle format: ${productHandle}`);
-    }
-
-    let serie = handleParts[1];
-
-    let handleRemainder = handleWithoutProductSlug.replace(
-      `iphone-${serie}`,
-      ''
-    );
-
-    let color: string | undefined = undefined;
-
-    if (expectedOptions.includes('color')) {
-      color = productColors.find((productColor) =>
-        handleRemainder.includes(productColor)
-      )!;
-      handleRemainder = handleRemainder.replace(color, '');
-    }
-
-    let tier: string | undefined = undefined;
-
-    if (expectedOptions.includes('tier')) {
-      tier = productTiers.find((productTier) =>
-        handleRemainder.includes(productTier)
-      )!;
-      handleRemainder = handleRemainder.replace(tier, '');
-    }
-
-    serie += handleRemainder.substring(0, handleRemainder.length - 1);
-
-    return { serie, color, tier };
-  }
-
-  private getDiscountPercentFromTags(tags: string[]): number | undefined {
-    const saleTag = tags.find((tag) =>
-      tag.toLocaleLowerCase().includes(discountedProductTagPrefix)
-    );
-
-    return saleTag ? parseFloat(saleTag.split('-')[1]) : undefined;
-  }
-
-  private applyDiscount(price: Money, discountPercent: number): Money {
-    const remainingPercent = 1 - discountPercent / 100;
-
-    return {
-      amount: Math.ceil(price.amount * remainingPercent),
-      currencyCode: price.currencyCode,
-    };
   }
 }
